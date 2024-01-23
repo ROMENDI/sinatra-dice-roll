9ccfe1ea9ca0ff8ec5f85f4ac1bc94d40a611890
Rohan Mendiratta <97857962+ROMENDI@users.noreply.github.com>
2024-01-23 05:50:30 +0000

    array looping

--- dice.rb
+++ dice.rb
@@ -21,39 +21,61 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
 	erb(:two_six)
 end
 
 # Route for rolling two 10-sided dice
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
   erb(:two_ten )
 end
 
 # Route for rolling one 20-sided die
 get("/dice/1/20") do
-  die = rand(1..20)
-  @outcome = "You rolled a #{die}."
+  @rolls = []
+
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+
   erb(:one_twenty)
 end
 
 # Route for rolling five 4-sided dice
 get("/dice/5/4") do
-  die1 = rand(1..4)
-  die2 = rand(1..4)
-  die3 = rand(1..4)
-  die4 = rand(1..4)
-  die5 = rand(1..4)
-  sum = die1 + die2 + die3 + die4 + die5
+  @rolls = []
 
-  @outcome = "You rolled #{die1}, #{die2}, #{die3}, #{die4}, and #{die5} for a total of #{sum}."
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
   erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
