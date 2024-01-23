ccd4134d25894bb8cde6384fb32686923207a64e
Rohan Mendiratta <97857962+ROMENDI@users.noreply.github.com>
2024-01-23 05:27:44 +0000

    added layout.erb

--- dice.rb
+++ dice.rb
@@ -34,15 +34,15 @@ get("/dice/2/10") do
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
-  "<h1>2d10</h1>
-  <p>You rolled a #{first_die} and a #{second_die} for a total of #{sum}.</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  erb(:two_ten )
 end
 
 # Route for rolling one 20-sided die
 get("/dice/1/20") do
   die = rand(1..20)
-  "<h1>1d20</h1>
-  <p>You rolled a #{die}.</p>"
+  @outcome = "You rolled a #{die}."
+  erb(:one_twenty)
 end
 
 # Route for rolling five 4-sided dice
@@ -54,6 +54,6 @@ get("/dice/5/4") do
   die5 = rand(1..4)
   sum = die1 + die2 + die3 + die4 + die5
 
-  "<h1>5d4</h1>
-  <p>You rolled #{die1}, #{die2}, #{die3}, #{die4}, and #{die5} for a total of #{sum}.</p>"
+  @outcome = "You rolled #{die1}, #{die2}, #{die3}, #{die4}, and #{die5} for a total of #{sum}."
+  erb(:five_four)
 end
